98845df5f417de2c17965f3d7e1966d2ea68f18d
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-07-01 10:52:07 +0200

    initial-commit

--- components/trendingMovies.js
+++ components/trendingMovies.js
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function trendingMovies() {
+export default function TrendingMovies() {
   return (
     <View>
       <Text>trendingMovies</Text>
